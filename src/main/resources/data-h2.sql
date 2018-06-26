149733a84408b6948d7c78b87fd6783be046b076
Slawek Babecki <slawomir.babecki@raiffeisen.pl>
2018-06-26 14:24:18 +0200

    zmiana pliku data-h2.sql

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,3 @@
 insert into author(id, firstName, lastName, email) values(1, 'Slawek5454', 'Babaecki', 'ssss@dfdsds.com');
 insert into author(id, firstName, lastName, email) values(2, 'Slawek1', 'Babaecki', 'ssss@dfdsds.com');
+insert into author(id, firstName, lastName, email) values(2, 'Slawek2', 'Babaecki', 'ssss@dfdsds.com');
